7137837da6457e2e8540e235d5d94232a1cf3bfa
Ayman Osman <aymano.osman@gmail.com>
2024-07-02 16:51:00 +0200

    implement core logic

--- lib/robots.ex
+++ lib/robots.ex
@@ -1,18 +1,65 @@
 defmodule Robots do
-  @moduledoc """
-  Documentation for `Robots`.
-  """
+  def move(robot, insts, grid, scents \\ MapSet.new())
 
-  @doc """
-  Hello world.
+  def move({x, y, dir}, [], _grid, _scents) do
+    {x, y, dir, :safe}
+  end
 
-  ## Examples
+  def move(robot, [inst | rest], grid, scents) do
+    case step(robot, inst, grid) do
+      {:ok, robot} ->
+        move(robot, rest, grid, scents)
 
-      iex> Robots.hello()
-      :world
+      {:error, _} ->
+        if safe?(robot, scents) do
+          move(robot, rest, grid, scents)
+        else
+          lost(robot)
+        end
+    end
+  end
 
-  """
-  def hello do
-    :world
+  def step(robot, :F, grid) do
+    new = forward(robot)
+
+    if out_of_bounds?(new, grid) do
+      {:error, new}
+    else
+      {:ok, new}
+    end
+  end
+
+  def step({x, y, dir}, tdir, _) when tdir in [:R, :L] do
+    {:ok, {x, y, turn(dir, tdir)}}
+  end
+
+  def forward({x, y, :N = dir}), do: {x, y + 1, dir}
+  def forward({x, y, :E = dir}), do: {x + 1, y, dir}
+  def forward({x, y, :S = dir}), do: {x, y - 1, dir}
+  def forward({x, y, :W = dir}), do: {x - 1, y, dir}
+
+  def turn(dir, :L), do: turn_left(dir)
+  def turn(dir, :R), do: turn_right(dir)
+
+  def turn_right(:N), do: :E
+  def turn_right(:E), do: :S
+  def turn_right(:S), do: :W
+  def turn_right(:W), do: :N
+
+  def turn_left(:N), do: :W
+  def turn_left(:E), do: :N
+  def turn_left(:S), do: :E
+  def turn_left(:W), do: :S
+
+  def out_of_bounds?({x, y, _}, {gx, gy}) do
+    x > gx or y > gy or x < 0 or y < 0
+  end
+
+  def lost({x, y, dir}) do
+    {x, y, dir, :lost}
+  end
+
+  def safe?({x, y, _}, scents) do
+    MapSet.member?(scents, {x, y})
   end
 end
--- test/robots_test.exs
+++ test/robots_test.exs
@@ -1,8 +1,34 @@
 defmodule RobotsTest do
   use ExUnit.Case
-  doctest Robots
 
-  test "greets the world" do
-    assert Robots.hello() == :world
+  # 5 3
+
+  # 1 1 E
+  # RFRFRFRF
+
+  # 3 2 N
+  # FRRFLLFFRRFLL
+
+  # 0 3 W
+  # LLFFFLFLFL
+
+  # 1 1 E
+  # 3 3 N LOST
+  # 2 3 S
+
+  test "move" do
+    grid = {5, 3}
+    assert {1, 1, :E, :safe} = Robots.move({1, 1, :E}, [:R, :F, :R, :F, :R, :F, :R, :F], grid)
+
+    assert {3, 3, :N, :lost} =
+             Robots.move({3, 2, :N}, [:F, :R, :R, :F, :L, :L, :F, :F, :R, :R, :F, :L, :L], grid)
+
+    assert {2, 3, :S, :safe} =
+             Robots.move(
+               {0, 3, :W},
+               [:L, :L, :F, :F, :F, :L, :F, :L, :F, :L],
+               grid,
+               MapSet.new([{3, 3}])
+             )
   end
 end
